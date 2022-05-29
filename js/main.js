5321c83cb8693a7c52cfd680c077d8f454b3d02d
Dmitriy <dberezin90@gmail.com>
2022-05-29 19:27:08 +0300

    Добавил адаптив для всех секций

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@ $(function(){
 	$('.carousel__inner').slick({
 		arrows:false,
 		dots:true,
-		slidesToShow: 3
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings:{
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 841,
+				settings:{
+					slidesToShow: 1
+				}
+			}
+		]
 	});
 
 });
